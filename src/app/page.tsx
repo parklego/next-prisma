2f754ba0ca257e3fa9ceda75c1e232b4039fee47
parklego <hbd9425@gmail.com>
2023-11-20 20:10:01 +0900

    서버 컴포넌트 연동하기
    클라이언트 컴포넌트와 서버 컴포넌트 연동하기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,44 @@
-export default function Home() {
-  return <main>init</main>;
+import prisma from "@/db";
+import Link from "next/link";
+import { TodoItem } from "../components/TodoItem";
+
+function getTodos() {
+  return prisma.todo.findMany();
+}
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server";
+
+  // console.log(id, complete);
+
+  await prisma.todo.update({ where: { id }, data: { complete } });
+}
+
+export default async function Home() {
+  const todos = await getTodos();
+
+  // const newTodo = await prisma.todo.create({
+  //   data: { title: "test", complete: false },
+  // });
+
+  // const deleteAllTodo = await prisma.todo.deleteMany();
+
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          href={"/new"}
+          className="border border-slate-300 text-slate-300 p-2 rounded hover:bg-slate-700"
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map((todo) => {
+          return <TodoItem key={todo.id} toggleTodo={toggleTodo} {...todo} />;
+        })}
+      </ul>
+    </>
+  );
 }
